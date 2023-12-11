46a41513f73076cb290cc7f4d62cd666d43dedaf
Mogyoródi Balázs <mogyorodi.balazs@dk.akkszalezi.hu>
2023-12-11 13:39:38 +0100

    print

--- src/jatek/Jatek.java
+++ src/jatek/Jatek.java
@@ -5,13 +5,17 @@ package jatek;
 public class Jatek {
     
     public Jatek(){
-    
-    
     }
     
     
     
        public void run() {
+           
+           System.out.println("hello");
+           
+           
+           
+           
         }
     
     
