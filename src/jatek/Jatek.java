2a9ad66d214e89179ff845eccc8e2377cd6c6db9
Mogyoródi Balázs <mogyorodi.balazs@dk.akkszalezi.hu>
2023-12-12 15:26:11 +0100

    javitas

--- src/jatek/Jatek.java
+++ src/jatek/Jatek.java
@@ -10,27 +10,49 @@ public class Jatek {
     public void run() {
         //   System.out.println("hello");
 
-        Karakter k1 = new Karakter("Horvát Dani", "Varázsló");
-        System.out.println(k1.toString());
+        Karakter karakter = new Karakter("Horvát Dani", "Varázsló");
+//        System.out.println(k1.toString());
+//        elvalaszto();
+//        k1.inventory();
+//        elvalaszto();
+//        k1.felvesz(new Targy("tigris kard", "kard"));
+//        k1.felvesz(new Targy("sima nyil", "nyil", "Lövésre használatos fegyver"), 8);
+//        k1.inventory();
+//        elvalaszto();
+//        k1.hasznal("sima nyil", 9);
+//        k1.hasznal("tigris kard");
+//        elvalaszto();
+//        k1.inventory();
+//        
+
+        karakter.felvesz(new Targy("d", "d leírás"), 2);
+        karakter.felvesz(new Targy("a", "Leírás"), 2);
+        karakter.felvesz(new Targy("d", "d leírás"), 2);
+        karakter.felvesz(new Targy("b", "b leírás"));
+        karakter.felvesz(new Targy("c", "c leírás"), 1);
+        karakter.felvesz(new Targy("d", "d leírás"), 2);
+        karakter.felvesz(new Targy("a", "Leírás"));
+        karakter.felvesz(new Targy("d", "d leírás"), 2);
+        karakter.felvesz(new Targy("a", "Leírás"));
+        karakter.felvesz(new Targy("d", "d leírás"), 2);
+        karakter.felvesz(new Targy("c", "c leírás"), 3);
+
+        karakter.inventory();
         elvalaszto();
-        k1.inventory();
+        System.out.println(karakter.toString());
+        karakter.hasznal("a");
+        karakter.hasznal("b", 2);
+        karakter.hasznal("c", 2, false);
+        karakter.hasznal("d", 5);
+        karakter.hasznal("x");
         elvalaszto();
-        k1.felvesz(new Targy("tigris kard", "kard"));
-        k1.felvesz(new Targy("sima nyil", "nyil", "Lövésre használatos fegyver"), 8);
-        k1.inventory();
-        elvalaszto();
-        k1.hasznal("sima nyil", 3);
-        k1.hasznal("tigris kard");
-        elvalaszto();
-        k1.inventory();
+        karakter.inventory();
 
     }
-    
-    
-    
-    public void elvalaszto(){
+
+    public void elvalaszto() {
         System.out.println("\n----------------------------------------------------------------------------------------------------------------------------------\n");
-    
+
     }
 
 }
--- src/jatek/Karakter.java
+++ src/jatek/Karakter.java
@@ -15,7 +15,7 @@ public class Karakter {
         felszereles = new ArrayList<>();
         hp = dobas() + 12;
         kepesseg = dobas() + 6;
-        szerencse = dobas() ;
+        szerencse = dobas();
         this.nev = nev;
         this.kaszt = kaszt;
     }
@@ -43,28 +43,35 @@ public class Karakter {
         this.hasznal(targy, mennyiseg, true);
     }
 
-    public void hasznal(String targy, int mennyiseg, boolean hasznal) {
-        int db = mennyiseg;
-        if (hasznal) {
-            int index = 0;
-            while (mennyiseg != 0 && index < felszereles.size()) {
-                if (felszereles.get(index).getNev() == targy) {
-                    felszereles.remove(felszereles.get(index));
-                    mennyiseg--;
+    public void hasznal(String targy, int mennyiseg, boolean elhasznal) {
+        int db = targy_inventory_megszamlalas(targy);
+        if (mennyiseg <= db) {
+            if (elhasznal) {
+                System.out.println("Használt fegyverek, amik elhasználodtak:");
+                for (int i = 0; i < felszereles.size(); i++) {
+                    if (felszereles.get(i).getNev() == targy) {
+                        System.out.println(felszereles.get(i).toString());
+                        felszereles.remove(felszereles.get(i));
+                    }
+
                 }
-                index++;
+            } else {
+                System.out.println("Használt fegyverek, amik nem használodtak el:");
+                for (int i = 0; i < felszereles.size(); i++) {
+                    if (felszereles.get(i).getNev() == targy) {
+                        System.out.println(felszereles.get(i).toString());
+
+                    }
+                }
             }
-            if (index >= felszereles.size()) {
-                System.out.println("Nincs ilyen tárgy az inventoryban.");
-            } 
-            else {
-                  if (db > 1){
-                  System.out.println("Elhasználtad a tárgyaidat.");
-                }else{
-                    System.out.println("Elhasználtad a tárgyadat.");
-                  }         
+        } else {
+            if (db == 0) {
+                System.out.println("Nincs ilyen tárgy az inventorydban");
+            } else {
+                System.out.println("Van ilyen tárgyad az inventorydban, de nincsen neked a kivánt mennyiséghez megfelelő.");
             }
         }
+
     }
 
     @Override
@@ -77,10 +84,21 @@ public class Karakter {
             System.out.println("Nincs egy tárgy semm az inventorydban.");
         } else {
             for (int i = 0; i < felszereles.size(); i++) {
-                System.out.println(   felszereles.get(i).toString());
-             
+                System.out.println(felszereles.get(i).toString());
+
             }
         }
 
     }
+
+    private int targy_inventory_megszamlalas(String targy) {
+        int mennyiseg = 0;
+        for (int i = 0; i < felszereles.size(); i++) {
+            if (felszereles.get(i).getNev() == targy) {
+                mennyiseg++;
+            }
+        }
+        return mennyiseg;
+    }
+
 }
